ec9b94088aa53049e400a79f15b68d0f42171a3f
Rebecca Bell <rebeccaromanobell@gmail.com>
2023-01-09 16:03:47 +0000

    added function for displaying user info

--- precourse_recap.py
+++ precourse_recap.py
@@ -1,6 +1,12 @@
 first_name = input("What is your first name?")
 last_name = input("What is your last name?")
-print("Your name is " + first_name + " " + last_name)
 birth_year = input("What year were you born?")
 age = (2023 - int(birth_year))
-print("You will be " + str(age) + " this year")
+
+
+def displayInfo():
+    print("Your name is " + first_name + last_name +
+          " and you will be " + str(age) + " this year")
+
+
+displayInfo()
